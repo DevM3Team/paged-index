1966674e4e21cf46602617b9e54b4b93068ba4b6
ares <ares.bagatin@m3tean.it>
2022-09-14 10:29:40 +0200

    Added the toArray to the collection in PagedIndexCollection to return the data in a simple array without the id of the object as key

--- src/SimplePagedIndex.php
+++ src/SimplePagedIndex.php
@@ -2,11 +2,12 @@
 
 namespace M3Team\PagedIndex;
 
+use Closure;
 use Illuminate\Support\Collection;
 
 /**
  * This type of paged index deals only with page's size and page's index. It does not make any sorting or filtering operation
- * Paged index che si occupa solamente di paginare una collection senza opeare operazioni di ordinamento e/o filtro
+ * Paged index che si occupa solamente di paginare una collection senza operare operazioni di ordinamento e/o filtro
  */
 class SimplePagedIndex extends PagedIndex
 {
@@ -14,16 +15,13 @@ class SimplePagedIndex extends PagedIndex
     /**
      * @inheritDoc
      */
-    protected function sort(): Collection
+    protected function filter(): Collection
     {
         return $this->collection;
     }
 
-    /**
-     * @inheritDoc
-     */
-    protected function filter(): Collection
+    protected function sortingFunction(): Closure
     {
-        return $this->collection;
+        return fn() => "";
     }
 }
