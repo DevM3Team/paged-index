054106bd7fa6d03b8d28f385e7fc66df0d1a27b6
Ares Bagatin <ares.bagatin@m3team.it>
2025-12-16 16:46:33 +0100

    New Paged Index

--- config/paged-index.php
+++ config/paged-index.php
@@ -4,7 +4,7 @@ return [
     'request_keys' => [
         'page_index' => env('PAGED_INDEX_KEY_PAGE_INDEX', 'page_index'),
         'page_size' => env('PAGED_INDEX_KEY_PAGE_SIZE', 'page_size'),
-        'filter' => env('PAGED_INDEX_KEY_FILTER', 'filter'),
+        'filters' => env('PAGED_INDEX_KEY_FILTER', 'filters'),
         'sort_column' => env('PAGED_INDEX_KEY_SORT_COLUMN', 'sort_column'),
         'sort_direction' => env('PAGED_INDEX_KEY_SORT_DIRECTION', 'sort_direction'),
         'relationships' => env('PAGED_INDEX_KEY_RELATIONSHIPS', 'with'),
